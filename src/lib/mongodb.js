3285b973ed78e21ac453e0379ecedd3fc84c3e92
fady malak zaki <fadymalak7716@gmail.com>
2024-08-21 17:44:18 +0300

    edit mongodb url

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -4,7 +4,7 @@ let mongoClient;
 let clientPromise;
 
 if (!clientPromise) {
-    mongoClient = new MongoClient("mongodb+srv://fadymalak7716:Kmrh2JUWqjJgttcp@blogs.yrj8f.mongodb.net/?retryWrites=true&w=majority&appName=blogs");
+    mongoClient = new MongoClient("mongodb+srv://fadymalak7716:Kmrh2JUWqjJgttcp@blogs.yrj8f.mongodb.net/?retryWrites=true&w=majority&ssl=true&tlsAllowInvalidCertificates=true&appName=blogs");
     clientPromise = mongoClient.connect();
     clientPromise.then()
 }
